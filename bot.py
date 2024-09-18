cf30fcfedfd553c391c5bdd46634e501e1442e5f
Waveplate <dissahc@pm.me>
2024-09-18 12:48:38 -0700

    make login and get_tweet async, improved error handling, download img2irc binary instead of compiling it

--- bot.py
+++ bot.py
@@ -1,12 +1,19 @@
 import json
 import re
 import ssl
-import time
 import subprocess
 import irc.bot
 import irc.connection
 from datetime import datetime
 from twikit import Client
+
+import asyncio
+import threading
+from queue import Queue
+import signal
+import sys
+
+twitter_client = Client('en-US')
 
 with open('config.json', 'r') as f:
     config = json.load(f)
@@ -16,21 +23,58 @@ if img2irc_path.returncode != 0:
     print("img2irc not found, disabling twitpic")
     config['bot']['twitPic'] = False
 
-twitter_client = Client('en-US')
-twitter_client.login(
-    auth_info_1=config['twitter']['username'],
-    auth_info_2=config['twitter']['email'],
-    password=config['twitter']['password']
-)
+class AsyncLoopThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.loop = asyncio.new_event_loop()
+        self.daemon = True
+
+    def run(self):
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_forever()
+
+    def stop(self):
+        self.loop.call_soon_threadsafe(self.loop.stop)
+
+async_loop_thread = AsyncLoopThread()
+async_loop_thread.start()
+loop = async_loop_thread.loop
+
+send_queue = Queue()
+
+async def async_login():
+    try:
+        huh = await twitter_client.login(
+            auth_info_1=config['twitter']['username'],
+            auth_info_2=config['twitter']['email'],
+            password=config['twitter']['password']
+        )
+        print("Login successful:", huh)
+        return True
+    except Exception as e:
+        print("Login failed:", e)
+        return False
+
+async def async_get_tweet(tweet_id):
+    try:
+        tweet = await twitter_client.get_tweet_by_id(tweet_id)
+        return tweet
+    except Exception as e:
+        print(f"Error fetching tweet {tweet_id}: {e}")
+        return None
 
 class TwitterIRCBot(irc.bot.SingleServerIRCBot):
-    def __init__(self, config):
+    def __init__(self, config, loop, send_queue):
         self.config = config
+        self.loop = loop
+        self.send_queue = send_queue
+        self.logged_in = False
+
         server = config['irc']['host']
         port = config['irc']['port']
         nickname = config['irc']['nick']
         realname = config['irc']['gecos']
-        
+
         if config['irc'].get('use_ssl', False):
             ssl_context = ssl.create_default_context()
             ssl_context.check_hostname = False
@@ -39,7 +83,20 @@ class TwitterIRCBot(irc.bot.SingleServerIRCBot):
             irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, realname, connect_factory=ssl_factory)
         else:
             irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, realname)
-    
+
+        self.sender_thread = threading.Thread(target=self.process_send_queue, daemon=True)
+        self.sender_thread.start()
+
+    def process_send_queue(self):
+        while True:
+            target, message = self.send_queue.get()
+            if message:
+                try:
+                    self.connection.privmsg(target, message)
+                except Exception as e:
+                    print(f"Error sending message to {target}: {e}")
+            self.send_queue.task_done()
+
     def on_welcome(self, connection, event):
         for channel in self.config['irc']['channels']:
             print(f"Joining {channel}")
@@ -50,31 +107,65 @@ class TwitterIRCBot(irc.bot.SingleServerIRCBot):
         args = message.split(' ')
         text = ' '.join(args[1:])
 
+        asyncio.run_coroutine_threadsafe(
+            self.handle_pubmsg(connection, event, message, text),
+            self.loop
+        )
+
+    async def handle_pubmsg(self, connection, event, message, text):
+        if not self.logged_in:
+            success = await async_login()
+            self.logged_in = success
+            if not success:
+                self.send_queue.put((event.target, "Failed to login to Twitter."))
+                return
+
         if message.startswith('!image '):
-            self.config['bot']['twitPic'] = text == 'on'
-            connection.privmsg(event.target, f"twitpic {'on' if self.config['bot']['twitPic'] else 'off'}")
+            self.config['bot']['twitPic'] = text.lower() == 'on'
+            response = f"twitpic {'on' if self.config['bot']['twitPic'] else 'off'}"
+            self.send_queue.put((event.target, response))
 
         elif message.startswith('!width '):
-            self.config['bot']['ansi']['width'] = int(text)
-            connection.privmsg(event.target, f"twitpic width {self.config['bot']['ansi']['width']}")
+            try:
+                self.config['bot']['ansi']['width'] = int(text)
+                response = f"twitpic width {self.config['bot']['ansi']['width']}"
+                self.send_queue.put((event.target, response))
+            except ValueError:
+                self.send_queue.put((event.target, "Invalid width value."))
 
         elif message.startswith('!len '):
-            self.config['bot']['maxTweetLength'] = int(text)
-            connection.privmsg(event.target, f"maxTweetLength {self.config['bot']['maxTweetLength']}")
+            try:
+                self.config['bot']['maxTweetLength'] = int(text)
+                response = f"maxTweetLength {self.config['bot']['maxTweetLength']}"
+                self.send_queue.put((event.target, response))
+            except ValueError:
+                self.send_queue.put((event.target, "Invalid maxTweetLength value."))
 
         elif message.startswith('!wrap '):
-            self.config['bot']['wrapLen'] = int(text)
-            connection.privmsg(event.target, f"wrapLen {self.config['bot']['wrapLen']}")
+            try:
+                self.config['bot']['wrapLen'] = int(text)
+                response = f"wrapLen {self.config['bot']['wrapLen']}"
+                self.send_queue.put((event.target, response))
+            except ValueError:
+                self.send_queue.put((event.target, "Invalid wrapLen value."))
 
         elif message.startswith('!delay '):
-            self.config['bot']['delay'] = float(text)
-            connection.privmsg(event.target, f"delay {self.config['bot']['delay']}")
+            try:
+                self.config['bot']['delay'] = float(text)
+                response = f"delay {self.config['bot']['delay']}"
+                self.send_queue.put((event.target, response))
+            except ValueError:
+                self.send_queue.put((event.target, "Invalid delay value."))
 
-        elif re.search(r'(twitter|x)\.com/.+?/status/\d+', message):
-            tweet_id = re.search(r'(twitter|x)\.com/.+?/status/(\d+)', message).group(2)
-            tweet = get_tweet(tweet_id)
-            if tweet:
-                draw_tweet(tweet, event, connection)
+        elif re.search(r'(twitter|x)\.com/.+?/status/(\d+)', message):
+            match = re.search(r'(twitter|x)\.com/.+?/status/(\d+)', message)
+            if match:
+                tweet_id = match.group(2)
+                tweet = await async_get_tweet(tweet_id)
+                if tweet:
+                    draw_tweet(tweet, event, connection, self.send_queue, self.config)
+                else:
+                    self.send_queue.put((event.target, "Failed to retrieve the tweet."))
 
 def wrap_text(input_text, line_length):
     paragraphs = input_text.split("\n")
@@ -101,7 +192,7 @@ def get_ansi(url, options):
     num_lines = len(result.stdout.split('\n'))
     return ansi_art, num_lines
 
-def append_multiline_strings(str1, str2, padding):
+def append_multiline_strings(str1, str2, padding, config):
     lines1 = str1.split('\n')
     lines2 = str2.split('\n')
     max_length = max(len(lines1), len(lines2))
@@ -115,52 +206,55 @@ def append_multiline_strings(str1, str2, padding):
 
     return '\n'.join(result).strip()
 
-def send_multiline_message(connection, target, message):
+def send_multiline_message(send_queue, target, message):
     for line in message.split('\n'):
-        connection.privmsg(target, line)
-        time.sleep(config['bot']['delay'])
+        send_queue.put((target, line))
 
-def draw_tweet(tweet, event, connection):
-    tweet_text = tweet.full_text if len(tweet.full_text) <= config['bot']['maxTweetLength'] else tweet.full_text[:config['bot']['maxTweetLength']] + "..."
-    twit_date = datetime.strptime(tweet.created_at, '%a %b %d %H:%M:%S %z %Y').strftime('%b %d %Y')
-    
-    stats = f"\x03{config['bot']['colors']['retweets']}{config['bot']['symbols']['retweets']} {tweet.retweet_count}\x03 "
-    stats += f"\x03{config['bot']['colors']['likes']}{config['bot']['symbols']['likes']} {tweet.favorite_count}\x03"
-
-    header = f"\x03{config['bot']['colors']['name']}\x1f\x02{tweet.user.name}\x02\x1f "
-    header += f"\x03{config['bot']['colors']['user']}@{tweet.user.screen_name} "
-    header += f"\x03{config['bot']['colors']['date']}{twit_date}\n"
-
-    wrapped = wrap_text(tweet_text, config['bot']['wrapLen'])
-    if config['bot']['colors']['text']:
-        wrapped = '\n'.join([f"\x03{config['bot']['colors']['text']}{line}\x03" for line in wrapped.split('\n')])
-    
-    text = header + wrapped
-
-    if config['bot']['twitPic']:
-        print("Profile Image URL:", tweet.user.profile_image_url)
-        ansi, ansi_height = get_ansi(tweet.user.profile_image_url, config['bot']['ansi'])
-
-        num_lines = len(text.split('\n')) + 1
-
-        if num_lines < ansi_height:
-            text += '\n' * (ansi_height - num_lines - 1)
-
-        text += f"\n{stats}"
-
-        message = append_multiline_strings(ansi, text, 1)
-    else:
-        text += f"\n{stats}"
-        message = text
-
-    send_multiline_message(connection, event.target, message)
-
-def get_tweet(tweet_id):
+def draw_tweet(tweet, event, connection, send_queue, config):
     try:
-        tweet = twitter_client.get_tweet_by_id(tweet_id)
-        return tweet
-    except Exception as e:
-        print(e)
+        text = getattr(tweet, 'full_text', tweet.text)
+        tweet_text = text if len(text) <= config['bot']['maxTweetLength'] else text[:config['bot']['maxTweetLength']] + "..."
 
-bot = TwitterIRCBot(config)
-bot.start()
+        twit_date = datetime.strptime(tweet.created_at, '%a %b %d %H:%M:%S %z %Y').strftime('%b %d %Y')
+        
+        stats = f"\x03{config['bot']['colors']['retweets']}{config['bot']['symbols']['retweets']} {tweet.retweet_count}\x03 "
+        stats += f"\x03{config['bot']['colors']['likes']}{config['bot']['symbols']['likes']} {tweet.favorite_count}\x03"
+
+        header = f"\x03{config['bot']['colors']['name']}\x1f\x02{tweet.user.name}\x02\x1f "
+        header += f"\x03{config['bot']['colors']['user']}@{tweet.user.screen_name} "
+        header += f"\x03{config['bot']['colors']['date']}{twit_date}\n"
+
+        wrapped = wrap_text(tweet_text, config['bot']['wrapLen'])
+        if config['bot']['colors']['text']:
+            wrapped = '\n'.join([f"\x03{config['bot']['colors']['text']}{line}\x03" for line in wrapped.split('\n')])
+        
+        text = header + wrapped
+
+        if config['bot']['twitPic']:
+            print("Profile Image URL:", tweet.user.profile_image_url)
+            ansi, ansi_height = get_ansi(tweet.user.profile_image_url, config['bot']['ansi'])
+
+            num_lines = len(text.split('\n')) + 1
+
+            if num_lines < ansi_height:
+                text += '\n' * (ansi_height - num_lines - 1)
+
+            text += f"\n{stats}"
+
+            message = append_multiline_strings(ansi, text, 1, config)
+        else:
+            text += f"\n{stats}"
+            message = text
+
+        send_multiline_message(send_queue, event.target, message)
+    except Exception as e:
+        print("Error drawing tweet:", e)
+        
+if __name__ == "__main__":
+    try:
+        bot = TwitterIRCBot(config, loop, send_queue)
+        bot.start()
+    except KeyboardInterrupt:
+        print("Shutting down bot...")
+        async_loop_thread.stop()
+        sys.exit(0)
